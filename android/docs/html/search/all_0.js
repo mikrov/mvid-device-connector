a91c640c2687c75cb02df22bd90b0ee2bb9e160a
mikrov <support@mikrov.dk>
2014-06-02 11:27:18 +0200

    Initial andoid support

---
+++ android/docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mviddeviceconnector',['MVIDDeviceConnector',['../classcom_1_1mvnordic_1_1mviddeviceconnector_1_1_m_v_i_d_device_connector.html',1,'com::mvnordic::mviddeviceconnector']]]
+];
---
+++ android/docs/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['res',['Res',['../classcom_1_1mvnordic_1_1mviddeviceconnector_1_1_res.html',1,'com::mvnordic::mviddeviceconnector']]]
+];
