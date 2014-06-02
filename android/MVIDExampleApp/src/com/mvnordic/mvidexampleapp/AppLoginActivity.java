3b4a76f221644ebcddc5f438b6d67b5d7ab7f801
mikrov <support@mikrov.dk>
2014-06-02 13:36:49 +0200

    New strings dialog

--- android/MVIDExampleApp/src/com/mvnordic/mvidexampleapp/AppLoginActivity.java
+++ android/MVIDExampleApp/src/com/mvnordic/mvidexampleapp/AppLoginActivity.java
@@ -1,18 +1,23 @@
 package com.mvnordic.mvidexampleapp;
 
 import com.mvnordic.mviddeviceconnector.DeviceSecurity;
+import com.mvnordic.mviddeviceconnector.DeviceSecurity.DeviceSecurityListener.MVIDResponse;
 
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 import android.os.Build;
 
 public class AppLoginActivity extends Activity {
@@ -50,6 +55,7 @@ public class AppLoginActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -60,21 +66,57 @@ public class AppLoginActivity extends Activity {
         public PlaceholderFragment() {
         }
 
+    	private DeviceSecurity.DeviceSecurityListener device_security_listener = new DeviceSecurity.DeviceSecurityListener() {
+
+    		@Override
+    		public void onMVIDResponseReady(MVIDResponse response) {
+    			String toast = String.format("AI: %s\nREQUEST_ID: %s\nGOT ACCESS: %s",
+    					response.access_identifier,
+    					response.request_id,
+    					response.has_access ? "YES" : "NO" );
+    			Toast.makeText(getView().getContext(), toast, 
+    					   Toast.LENGTH_SHORT).show();
+    		}
+    		
+    	};
+
+        
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
+        	
             m_device_security = new DeviceSecurity(this.getActivity());
+            m_device_security.addDeviceSecurityListener(device_security_listener);
+            
             View rootView = inflater.inflate(R.layout.fragment_app_login, container, false);
             Button login_button = (Button) rootView.findViewById(R.id.mvid_login_btn);
+            
+    		final Spinner spinner = (Spinner) rootView.findViewById(R.id.ai_spinner);
+    		// Create an ArrayAdapter using the string array and a default spinner layout
+    		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(),
+    		        R.array.list_of_ais, android.R.layout.simple_spinner_item);
+    		// Specify the layout to use when the list of choices appears
+    		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    		// Apply the adapter to the spinner
+    		spinner.setAdapter(adapter);
+
             OnClickListener login_listener = new OnClickListener() {
 
 				@Override
-				public void onClick(View arg0) {
-					m_device_security.doLogin("self.modify");
+				public void onClick(View v) {
+					m_device_security.doLogin(spinner.getSelectedItem().toString());
 				}
             	
             };
             login_button.setOnClickListener(login_listener);
+
+    		Button release_button = (Button) rootView.findViewById(R.id.release_device_hash);
+    		release_button.setOnClickListener(new OnClickListener() {
+    			public void onClick(View v) {
+    				m_device_security.releaseDeviceRegistration();
+    			}
+    		});
+
             return rootView;
         }
     }
